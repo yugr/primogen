05ad5aaa90df3db0c3f3f47599d73e4f107e9b9d
Yury Gribov <tetra2005@gmail.com>
2016-07-12 07:46:55 +0100

    Fast encoder now synthesizable (disabled by default).

--- src/prio_enc.v
+++ src/prio_enc.v
@@ -7,43 +7,8 @@ module prio_enc #(
 
 localparam WIDTH = 1 << WIDTH_LOG;
 
-/* Concise but not supported by Icarus BLIF backend.
-
-function [7:0] get_msb;
-  input [HI:0] x;
-  reg [7:0] bit;
-  integer i;
-  begin
-    bit = 0;
-    // TODO: this could be done by splitting in halves on each iteration
-    for (i = HI; i >= 0; i = i - 1)
-      if (!bit && x[i])
-        bit = i;
-    get_msb = bit;
-  end
-endfunction
-
-always @*
-  msb = get_msb(a);
-*/
-
-/* Fast but not synthesizable by Icarus BLIF backend
-
-integer start, width;
-reg [7:0] msb;
-
-always @* begin
-  start = 0;
-  width = WIDTH;
-  while (width > 1) begin
-    width = width >> 1;
-    start = (|(x >> width)) ? (start + width) : start;
-  end
-  msb = start;
-end
-*/
-
-// Slow but Icarus fails at anything more complicated
+`ifndef FAST_ENCODER
+// Slow but simple
 
 integer i;
 reg [7:0] msb;
@@ -54,5 +19,21 @@ always @* begin
     if (!msb && x[i])
       msb = i;
 end
+`else
+// Faster
+
+integer i, start, width;
+reg [7:0] msb;
+
+always @* begin
+  start = 0;
+  width = WIDTH;
+  for (i = 0; i < WIDTH_LOG - 1; i = i + 1) begin
+    width = width >> 1;
+    start = (|(x >> width)) ? (start + width) : start;
+  end
+  msb = start;
+end
+`endif
 
 endmodule
