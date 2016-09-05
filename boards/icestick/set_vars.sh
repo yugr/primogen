17e9bb8e9e79df978eb94157aa8493d8a6c3b145
Yury Gribov <tetra2005@gmail.com>
2016-09-05 21:26:15 +0100

    Read license file location from iCEcube config.

--- boards/icestick/set_vars.sh
+++ boards/icestick/set_vars.sh
@@ -1,12 +1,32 @@
 #!/bin/sh -e
+# This file intentionally lacks executable perms
+# (to force you to source it).
 
-test $# = 2
+test $# = 1
 
 ICE_ROOT=$1
-LM_LICENSE_FILE=$2
+
+CFG=$ICE_ROOT/config.xml
+
+if ! test -f $CFG; then
+  echo >&2 "Config file missing in $ICE_ROOT"
+  exit 1
+fi
+
+LM_LICENSE_FILE=$(sed -ne '/LM_LICENSE_FILE/{ s/.*>\(.*\)<.*/\1/; p }' $CFG | tr '\' '/' )
+
+if test -z "$LM_LICENSE_FILE"; then
+  echo >&2 "Failed to find license info in $CFG"
+  exit 1
+fi
 
 if ! test -f $LM_LICENSE_FILE; then
-  echo >&2 "License file not found at $LM_LICENSE_FILE (need to update LM_LICENSE_FILE env. var.?)"
+  echo >&2 "License file not found at $LM_LICENSE_FILE"
+  exit 1
+fi
+
+if test -z "$LM_LICENSE_FILE"; then
+  echo >&2 "TCL library info not found in $CFG"
   exit 1
 fi
 
@@ -21,6 +41,9 @@ test -x $SIM
 
 PROG=$ICE_ROOT/../Programmer/3.7_x64/bin/nt64/pgrcmd
 
+# Should be something like $ICE_ROOT/sbt_backend/bin/win32/lib/tcl8.4
+export TCL_LIBRARY=$(sed -ne '/TCL_LIBRARY/ { s!.*>\(.*\)<.*!\1!; s!\$(RootPath)!'$ICE_ROOT'!; p }' $CFG)
+
 # We need any Windows TCL here (Cygwin's TCL will not work)
 TCL=$ICE_ROOT/Aldec/Active-HDL/BIN/tclsh85
 test -x $TCL
@@ -31,7 +54,6 @@ export LM_LICENSE_FILE=$LM_LICENSE_FILE
 export PLATFORM=MCD
 export SBT_DIR=$ICE_ROOT/sbt_backend
 export SYNPLIFY_PATH=$ICE_ROOT/synpbase
-export TCL_LIBRARY=$ICE_ROOT/sbt_backend/bin/win32/lib/tcl8.4
 
 if test $(uname -o) = Cygwin; then
   for v in FOUNDRY LM_LICENSE_FILE SBT_DIR SYNPLIFY_PATH TCL_LIBRARY; do
