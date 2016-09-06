b444f1f012cbc7f77e1334895fc22947a530701d
Yury Gribov <tetra2005@gmail.com>
2016-09-06 22:01:23 +0100

    Get as much as possible from config.xml.

--- boards/icestick/set_vars.sh
+++ boards/icestick/set_vars.sh
@@ -2,7 +2,10 @@
 # This file intentionally lacks executable perms
 # (to force you to source it).
 
-test $# = 1
+if test $# != 1; then
+  echo >&2 "Syntax: source set_vars.sh ICE_ROOT"
+  exit 1
+fi
 
 ICE_ROOT=$1
 
@@ -13,51 +16,53 @@ if ! test -f $CFG; then
   exit 1
 fi
 
-LM_LICENSE_FILE=$(sed -ne '/LM_LICENSE_FILE/{ s/.*>\(.*\)<.*/\1/; p }' $CFG | tr '\' '/' )
+# From this we'll get LM_LICENSE_FILE, SBT_DIR and TCL_LIBRARY
+for var in $(sed -ne '/<Environment>/,/<\/Environment>/{ />.*</{ s!.*<\([A-Za-z_0-9]\+\).*>\(.*\)<.*!\1=\2! ; s!\$(RootPath)!'$ICE_ROOT'! ; p } }' $CFG | tr '\' '/'); do
+  eval "export $var"
+done
 
-if test -z "$LM_LICENSE_FILE"; then
-  echo >&2 "Failed to find license info in $CFG"
+if test -z "$LM_LICENSE_FILE" -o -z "$TCL_LIBRARY" -o -z "$SBT_DIR"; then
+  echo >&2 "Failed to parse $CFG"
   exit 1
 fi
 
-if ! test -f $LM_LICENSE_FILE; then
-  echo >&2 "License file not found at $LM_LICENSE_FILE"
-  exit 1
-fi
-
-if test -z "$LM_LICENSE_FILE"; then
-  echo >&2 "TCL library info not found in $CFG"
-  exit 1
-fi
-
-SYNTH_LSE=$ICE_ROOT/LSE/bin/nt/synthesis
-test -x $LSE_SYNTH
-
-SYNTH_SYN=$ICE_ROOT/sbt_backend/bin/win32/opt/synpwrap/synpwrap
-test -x $SYN_SYNTH
-
-SIM=$ICE_ROOT/Aldec/Active-HDL/BIN/VSimSA
-test -x $SIM
-
-PROG=$ICE_ROOT/../Programmer/3.7_x64/bin/nt64/pgrcmd
-
-# Should be something like $ICE_ROOT/sbt_backend/bin/win32/lib/tcl8.4
-export TCL_LIBRARY=$(sed -ne '/TCL_LIBRARY/ { s!.*>\(.*\)<.*!\1!; s!\$(RootPath)!'$ICE_ROOT'!; p }' $CFG)
-
-# We need any Windows TCL here (Cygwin's TCL will not work)
-TCL=$ICE_ROOT/Aldec/Active-HDL/BIN/tclsh85
-test -x $TCL
-
-# These variables need to be set for Lattice tools
+# Other variables that need to be set for Lattice tools
 export FOUNDRY=$ICE_ROOT/LSE
-export LM_LICENSE_FILE=$LM_LICENSE_FILE
-export PLATFORM=MCD
-export SBT_DIR=$ICE_ROOT/sbt_backend
 export SYNPLIFY_PATH=$ICE_ROOT/synpbase
+#export PLATFORM=MCD
 
 if test $(uname -o) = Cygwin; then
   for v in FOUNDRY LM_LICENSE_FILE SBT_DIR SYNPLIFY_PATH TCL_LIBRARY; do
     eval "$v=\`cygpath -w \$$v\`"
   done
+fi
+
+# Tool paths (for Makefile)
+
+SYNTH_LSE=$ICE_ROOT/LSE/bin/nt/synthesis
+if ! test -x $SYNTH_LSE; then
+  echo >&2 "Lattice synthesizer not found in $SYNTH_LSE"
+  exit 1
+fi
+
+SYNTH_SYN=$ICE_ROOT/sbt_backend/bin/win32/opt/synpwrap/synpwrap
+if ! test -x $SYNTH_SYN; then
+  echo >&2 "Synplify not found in $SYNTH_SYN"
+  exit 1
+fi
+
+SIM=$ICE_ROOT/Aldec/Active-HDL/BIN/VSimSA
+if ! test -x $SIM; then
+  echo >&2 "Active-HDL simulator not found in $SIM"
+#  exit 1
+fi
+
+PROG=$ICE_ROOT/../Programmer/3.7_x64/bin/nt64/pgrcmd
+
+# We need any Windows TCL here (Cygwin's TCL will not work)
+TCL=$ICE_ROOT/Aldec/Active-HDL/BIN/tclsh85
+if ! test -x $TCL; then
+  echo >&2 "TCL not found at $TCL"
+  exit 1
 fi
 
