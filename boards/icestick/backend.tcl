d6739bb8772539677e03af6081cdf579e04906c6
Yury Gribov <tetra2005@gmail.com>
2016-09-15 22:19:31 +0100

    Refactor backend script.

--- boards/icestick/backend.tcl
+++ boards/icestick/backend.tcl
@@ -1,14 +1,12 @@
-set device iCE40HX1K-TQ144
-set proj_dir [pwd]
-set top_module [lindex $argv 0]
-set edif_file $top_module
-set tool_options ":edifparser --physicalconstraint syn/icestick.pcf"
-
 set sbt_root $::env(SBT_DIR)
 append sbt_tcl $sbt_root "/tcl/sbt_backend_synpl.tcl"
 source $sbt_tcl
 
-run_sbt_backend_auto $device $top_module $proj_dir "bin" $tool_options $edif_file
+set device iCE40HX1K-TQ144
+set top_module [lindex $argv 0]
+set proj_dir [pwd]
+set tool_options ":edifparser --physicalconstraint syn/icestick.pcf"
+set edif_file $top_module
 
-exit
+run_sbt_backend_auto $device $top_module $proj_dir "bin" $tool_options $edif_file
 
